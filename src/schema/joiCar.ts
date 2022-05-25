934a33f1cebc80b1ff82991b163d64df1db402e9
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-25 20:28:43 -0300

    fix: linter joi schema

--- src/schema/joiCar.ts
+++ src/schema/joiCar.ts
@@ -1,23 +1,26 @@
 import Joi from 'joi';
 
+const required = '400| is required';
+const typeNumber = '400| must be a number';
+
 export default Joi.object({
   _id: Joi.string(),
   model: Joi.string().required().min(3)
     .messages({
-      'any.required': '400| is required',
+      'any.required': required,
       'string.base': '400| must be a string',
       'string.min': '400| must be greater than 2',
     }),
   year: Joi.number().required().min(1900).max(2022)
     .messages({
-      'any.required': '400| is required',
-      'number.base': '400| must be a number',
+      'any.required': required,
+      'number.base': typeNumber,
       'number.min': '400| must be greater than 1899',
       'number.max': '400| must be less than 2023',
     }),
   color: Joi.string().required().min(3)
     .messages({
-      'any.required': '400| is required',
+      'any.required': required,
       'string.base': '400| must be a string',
       'string.min': '400| must be greater than 2',
     }),
@@ -30,15 +33,15 @@ export default Joi.object({
     }),
   doorsQty: Joi.number().required().min(2).max(4)
     .messages({
-      'any.required': '400| is required',
-      'number.base': '400| must be a number',
+      'any.required': required,
+      'number.base': typeNumber,
       'number.min': '400| must be greater than 1',
       'number.max': '400| must be less than 5',
     }),
   seatsQty: Joi.number().required().min(2).max(7)
     .messages({
-      'any.required': '400| is required',
-      'number.base': '400| must be a number',
+      'any.required': required,
+      'number.base': typeNumber,
       'number.min': '400| must be greater than 1',
       'number.max': '400| must be less than 8',
     }),
