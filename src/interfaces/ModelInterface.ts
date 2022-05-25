94a2073f4145ef34d54f37d8ff9c59e51be9207f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-25 15:09:51 -0300

    feat: interface generic model - req01 done

---
+++ src/interfaces/ModelInterface.ts
@@ -0,0 +1,8 @@
+export interface Model<T> {
+  create(obj: T): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(id_: string): Promise<T | null>,
+  update(str: string, obj: T): Promise<T | null>,
+  delete(str: string): Promise<T | null>,
+
+}
