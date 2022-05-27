215394c87a97091103449f9753fbcf00b404feda
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-27 12:01:33 -0300

    improve: response error middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -5,8 +5,7 @@ const error: ErrorRequestHandler = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || 'Something went wrong';
 
-  console.error(message);
-  return res.status(status).json({ message });
+  return res.status(status).json({ error: message });
 };
 
 export default error;
