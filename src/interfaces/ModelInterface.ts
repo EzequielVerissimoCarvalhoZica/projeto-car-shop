86140b025879033021168a88d6e7d7ef97711b7b
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-27 11:11:44 -0300

    feat: add method in model interface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,6 +1,6 @@
 export interface Model<T> {
   create(obj: T): Promise<T>,
-  // read(): Promise<T[]>,
+  read(): Promise<T[]>,
   // readOne(id_: string): Promise<T | null>,
   // update(str: string, obj: T): Promise<T | null>,
   // delete(str: string): Promise<T | null>,
