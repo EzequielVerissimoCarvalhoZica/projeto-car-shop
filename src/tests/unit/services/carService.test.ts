334dffec1a0541cb7e462d0042c32a989069d423
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-27 17:12:58 -0300

    tests: service method read and readOne

--- src/tests/unit/services/carService.test.ts
+++ src/tests/unit/services/carService.test.ts
@@ -14,6 +14,25 @@ const carMock = {
   _id: '628fc7176f39b697bcd474b8',
 }
 
+const carsMock = [{
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2,
+  _id: '628fc7176f39b697bcd474b5',
+},
+{
+  model: 'Audi TT',
+  year: 2020,
+  color: 'blue',
+  buyValue: 286127,
+  seatsQty: 2,
+  doorsQty: 2,
+  _id: '628fc7176f39b697bcd473a2',
+}] as (CarDocument & { _id: any; })[]
+
 const carCreate = {
   model: 'Uno da Escada',
   year: 1963,
@@ -47,6 +66,38 @@ describe(('Test car service'), () => {
       const created = await carService.create(carMock);
 
       expect(created).to.deep.eq(carMock);
+    })
+  })
+  describe(('Read all cars'), () => {
+    const carModel = new CarModel(mongooseModelMock);
+    const carService = new CarService(carModel);
+
+    before(() => {
+      Sinon.stub(carModel, 'read').resolves(carsMock)
+    })
+    after(() => {
+      (carModel.read as Sinon.SinonStub).restore();
+    })
+    it('Return cars', async () => {
+      const cars = await carService.read();
+
+      expect(cars).to.deep.eq(carsMock);
+    })
+  })
+  describe(('Read car by id'), () => {
+    const carModel = new CarModel(mongooseModelMock);
+    const carService = new CarService(carModel);
+
+    before(() => {
+      Sinon.stub(carModel, 'readOne').resolves(carMock)
+    })
+    after(() => {
+      (carModel.readOne as Sinon.SinonStub).restore();
+    })
+    it('Return car by id', async () => {
+      const car = await carService.readOne('628fc7176f39b697bcd474b8');
+
+      expect(car).to.deep.eq(carMock);
     })
   })
 })
