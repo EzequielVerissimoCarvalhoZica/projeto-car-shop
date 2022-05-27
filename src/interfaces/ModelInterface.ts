ec6c050cda0e62e7391f8d7bad82284cf8a2e22c
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-27 12:02:05 -0300

    feat: add method read one in model interface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
-  // readOne(id_: string): Promise<T | null>,
+  readOne(id_: string): Promise<T | null>,
   // update(str: string, obj: T): Promise<T | null>,
   // delete(str: string): Promise<T | null>,
 }
