e5c6c390af5bc3d3455d1e7e23255ebecd36a49b
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-27 12:03:18 -0300

    feat: add method read one car in controller

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,12 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { Car } from '../interfaces/CarInterface';
 import CarService from '../services/CarService';
+import HttpException from '../schema/HttpException';
 
 export interface RequestWithBody extends Request {
   body: Car;
 }
 
 export default class CarController {
+  static _httpException: HttpException;
+
   constructor(
     private _CarService = new CarService(),
     private _route = '/cars',
@@ -26,5 +29,23 @@ export default class CarController {
     const cars = await this._CarService.read();
 
     return res.status(200).json(cars);
+  };
+
+  readOne = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    if (id.length !== 24) {
+      return next(
+        new HttpException(400, 'Id must have 24 hexadecimal characters'),
+      );
+    }
+
+    const car = await this._CarService.readOne(id);
+
+    if (!car) {
+      return next(new HttpException(404, 'Object not found'));
+    }
+
+    return res.status(200).json(car);
   };
 }
