929e750286c0eef57b7dee6aa33a6fe69a0cfd40
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-05-30 18:22:33 -0300

    refactor: middle validate create car

--- src/middlewares/validateCreateCar.ts
+++ src/middlewares/validateCreateCar.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '../schema/HttpException';
-import joiCar from '../schema/joiCar';
+import joiCreateCar from '../schema/joiCreateCar';
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const { body } = req;
 
-  const { error } = joiCar.validate({ ...body });
+  const { error } = joiCreateCar.validate({ ...body });
 
   if (error) {
     const [code, message] = error.message.split('|');
